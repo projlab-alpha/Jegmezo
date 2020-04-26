d6a7a5ed6edb3f49472d7945086ceb8f0ac0b092
Bálint Nagy <info.balint.nagy@edu.bme.hu>
2020-04-26 11:32:15 +0200

    Added Commands, Prototype (program entry point) classes, added command skeletons to Commands

---
+++ Jegmezo/src/control/Prototype.java
@@ -0,0 +1,11 @@
+package control;
+
+/**
+ * A prototípus tesztelését végrehajtó osztály,
+ * és a program jelenlegi belépési pontja.
+ */
+public class Prototype {
+    public static void main(String[] args) {
+
+    }
+}

6b74acfdafb18efedea403e4482e6f438a13f4a2
Bálint Nagy <info.balint.nagy@edu.bme.hu>
2020-05-08 15:32:26 +0200

    Added

---
+++ Jegmezo/src/field/FieldAppearance.java
@@ -0,0 +1,17 @@
+package field;
+
+/**
+ * Egyszerű adatstruktúra a getterek számának csökkentése érdekében,
+ * a DisplayTile használja a mezők státuszának lekérdezésére.
+ */
+public class FieldAppearance {
+    public boolean isHole = false;
+    public boolean isUnstable = false;
+    public boolean hasSnow = false;
+    public boolean hasItem = false;
+    public boolean hasEskimo = false;
+    public boolean hasResearcher = false;
+    public boolean hasBear = false;
+    public boolean hasIgloo = false;
+    public boolean hasTent = false;
+}
